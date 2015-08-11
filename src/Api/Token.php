9ae89bf78982fd9c772b797c84b032f92c629fec
Jesse Vista <jesse@codegreencreative.com>
2015-08-11 14:17:22 -0400

    Added two API classes

---
+++ src/Api/Token.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Upwebdesign\Grovo\Api
+
+use Upwebdesign\Grovo\Http\GuzzleClient;
+
+/**
+*
+*/
+class Token extends GuzzleClient
+{
+    /**
+     * [$endpoint description]
+     * @var string
+     */
+    private $endpoint = 'token';
+
+    // To update the config file with new token
+    // File::put(app_path() . '/config/customization.php', "<?php\n return $data ;")
+
+    /**
+     * [__construct description]
+     */
+    public function __construct()
+    {
+        if (empty(config('token'))) {
+            return $this->request($endpoint, [
+                'client_id' => $this->client_id,
+                'client_secret' => $this->client_secret,
+                'grant_type' => 'client_credentials'
+            ]);
+        }
+    }
+
+}
---
+++ src/Api/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Upwebdesign\Grovo\Api
+
+use Upwebdesign\Grovo\Http\GuzzleClient;
+
+/**
+*
+*/
+class User extends GuzzleClient
+{
+    /**
+     * [$endpoint description]
+     * @var string
+     */
+    private $endpoint = 'users';
+
+    /**
+     * [get description]
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public function get($id=null)
+    {
+        if (is_null($id) || ! is_numeric($id)) {
+            throw new HttpException('User ID for this request is invalid!');
+        }
+        return $this->request(sprintf('%s/%s', $endpoint, $id));
+    }
+
+    /**
+     * [create description]
+     * @param  array  $data [description]
+     * @return [type]       [description]
+     */
+    public function create($data=[])
+    {
+        if (empty($data) || ! is_array($data)) {
+            throw new HttpException('You must supply user information to create a user!');
+        }
+        $data['data'] = [
+            'type' => 'users',
+            'attributes' => $data
+        ]
+        return $this->request($endpoint, $data);
+    }
+
+    /**
+     * [update description]
+     * @param  array  $data [description]
+     * @return [type]       [description]
+     */
+    public function update($id, $data=[])
+    {
+        if (empty($data) || ! is_array($data)) {
+            throw new HttpException('You must supply user information to create a user!');
+        }
+        $data['data'] = [
+            'type' => 'users',
+            'attributes' => $data
+        ]
+        return $this->method('PATCH')->request(sprintf('%s/%s', $endpoint, $id), $data);
+    }
+
+    /**
+     * [delete description]
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public function delete($id=null)
+    {
+        if (is_null($id) || ! is_numeric($id)) {
+            throw new HttpException('User ID for this request is invalid!');
+        }
+        return $this->method('DELETE')->request(sprintf('%s/%s', $endpoint, $id));
+    }
+}
