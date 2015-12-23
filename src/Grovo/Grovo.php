fbcd3de00ad2cd9efbcd73be6e879c06419786c1
Jesse Vista <jvista@upwebdesign.com>
2015-12-23 12:36:15 -0500

    attempt different approach

--- src/Grovo/Grovo.php
+++ src/Grovo/Grovo.php
@@ -19,7 +19,7 @@ use Cache;
 * https://grovo-api.readme.io/v1.0/docs
 * http://docs.grovo.apiary.io
 */
-class Grovo
+class Grovo extends GrovoApi
 {
     private $instance;
 
@@ -28,24 +28,28 @@ class Grovo
         // Get access token from cache, or false
         $access_token = ! Cache::has('grovo:access_token') ? false : Cache::get('grovo:access_token');
         // Set up GrovoApi
-        $this->instance = new GrovoApi(config('grovo.client_id'), config('grovo.client_secret'), $access_token, function($new_token) {
+        // $this->instance = new GrovoApi(config('grovo.client_id'), config('grovo.client_secret'), $access_token, function($new_token) {
+        //     $expiresAt = Carbon::now()->addDay();
+        //     Cache::put('grovo:access_token', $new_token, $expiresAt); // Gobal
+        // });
+        // If not debugging set to live api and auth URLs
+        if ( ! config('grovo.debug')) {
+            $this->setApiUrl('https://api.grovo.com');
+            $this->setAuthUrl('https://api.grovo.com');
+            // $this->instance->setAuthUrl('https://auth.grovo.com');
+        }
+        parent::__construct(config('grovo.client_id'), config('grovo.client_secret'), $access_token, function($new_token) {
             $expiresAt = Carbon::now()->addDay();
             Cache::put('grovo:access_token', $new_token, $expiresAt); // Gobal
         });
-        // If not debugging set to live api and auth URLs
-        if ( ! config('grovo.debug')) {
-            $this->instance->setApiUrl('https://api.grovo.com');
-            $this->instance->setAuthUrl('https://api.grovo.com');
-            // $this->instance->setAuthUrl('https://auth.grovo.com');
-        }
     }
 
     /**
      * [getInstance description]
      * @return [type] [description]
      */
-    protected function getInstance()
-    {
-        return $this->instance;
-    }
+    // protected function getInstance()
+    // {
+    //     return $this->instance;
+    // }
 }
